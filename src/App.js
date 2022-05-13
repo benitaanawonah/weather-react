ad80c72f090c52ce88abfd7c70f0405efeabbd73
Benita Anawonah <benitaanawonah33@gmail.com>
2022-05-13 01:28:54 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
           React Debugging
         </h1>
         <br />
-        <a href="https://www.youtube.com/watch?v=ra1Jl3O_u6Q" target="_blank" rel="noreferrer">BCS</a>
-        <Weather city="Benin" />
+        
+        <Weather city="Lagos" />
       </header>
       
     </div>
